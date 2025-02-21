bddf90f0f86ad5fdde30606df9f2772a9e1f6027
Khokhar Deepkumar Kishorbhai <deepkhokhar1262004@gmail.com>
2025-02-21 12:28:52 +0530

    Pages added
    Add Home, Login, Signup Page with responsiveness and connect urls.

---
+++ legalize/legalize/views.py
@@ -0,0 +1,13 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    #return  HttpResponse("Hello, world. you are at first_project Home page")
+    return render(request,'website/index.html')
+
+def about(request):
+    #return  HttpResponse("Hello, world. you are at first_project about page")
+    return render(request, 'website/about.html')
+
+def contact(request):
+    return  HttpResponse("Hello, world. you are at first_project contact page") 

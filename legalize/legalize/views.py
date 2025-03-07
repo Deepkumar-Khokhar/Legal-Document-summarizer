7d2f39b3dddc98b7acb69dc6b5ab7fbd89a2144d
Deep-khokhar <deepkhokhar1262004@gmail.com>
2025-03-07 22:54:04 +0530

    version 1 kenil

--- legalize/legalize/views.py
+++ legalize/legalize/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
-def home(request):
+def landing(request):
     #return  HttpResponse("Hello, world. you are at first_project Home page")
     return render(request,'website/index.html')
 
